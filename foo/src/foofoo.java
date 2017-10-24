ae0af8627abd5a496956f89dedf3c33bcc709cf2
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2017-10-24 13:17:30 -0400

    10_24

--- foo/src/foofoo.java
+++ foo/src/foofoo.java
@@ -2,6 +2,11 @@
 public class foofoo {
 	public static void main(String[]args)
 	{
+		int a = 3;
+		int b = doubled(a);
+		System.out.println(a);
+		System.out.println(b);
+		
 		String foo = "foo";
 		String foofoo = doubled(foo);
 		System.out.println(foo);
@@ -9,13 +14,40 @@ public class foofoo {
 		
 		String[]testA = {"hi","bye"};
 		String[]testB = doubled(testA);
-		System.out.println(testA);
+		printArr(testA);
+		printArr(testB);
 		
+	}
+	public static int doubled(int x)
+	{
+		x = x+x;
+		return x;
 	}
 	public static String doubled(String str1)
 	{
 		str1 = str1 +str1;
 		return str1;
+	}
+	public static String[] doubled(String[]str2)
+	{
+		for(int i = 0; i < str2.length; i ++)
+		{
+			str2[i] = doubled(str2[i]);
+		}
+		return str2;
+	}
+	public static void printArr(Object[]arr)
+	{
+		for(Object x : arr)
+		{
+			System.out.println(x);
+		}
+		/*
+		for(int i = 0; i<arr.length; i++)
+		{
+			System.out.println(arr[i]);
+		}
+		*/
 	}
 
 }
